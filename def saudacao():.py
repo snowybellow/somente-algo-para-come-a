c2d292e268493925f95142b4938e615979306a13
Snowy <gabrielar2007a@gmail.com>
2026-03-01 18:09:46 -0300

    Add files via upload

---
+++ def saudacao():.py
@@ -0,0 +1,8 @@
+nome = input ("Qual o nome do seu filho ou filha?")
+sera_mesmo = input ("Esse é mesmo sua criança?")
+if sera_mesmo == "sim":
+    print ("Bem, sua criança disse que te ama")
+else:
+    print ("foi triste então;-;")
+    
+    #caso queiram usa com a mãe ou pai, de vocês podem.
